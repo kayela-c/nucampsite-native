210c87fab267bf2ea80423f8305e6f8b81dc5753
Kayela Claybon <34044644+kayela-c@users.noreply.github.com>
2024-06-20 16:33:20 -0500

    added featured item need to fix bug

--- features/FeaturedItem.js
+++ features/FeaturedItem.js
@@ -1,13 +1,5 @@
-import { useState } from 'react'
-import {
-    ScrollView,
-    Text,
-    View
-} from 'react-native-gesture-handler';
 import { Card } from 'react-native-elements';
-import { CAMPSITES } from '../shared/campsites'
-import { PROMOTIONS } from '../shared/promotions'
-import { PARTNERS } from '../shared/partners'
+import { Text, View } from 'react-native-gesture-handler';
 
 const FeaturedItem = ({ item }) => {
     if (item) {
@@ -40,23 +32,4 @@ const FeaturedItem = ({ item }) => {
     return <View />
 };
 
-
-const HomeScreen = () => {
-    const [campsites, setCampsites] = useState(CAMPSITES);
-    const [promotions, setPromotions] = useState(PROMOTIONS);
-    const [partners, setPartners] = useState(PARTNERS);
-
-    const featCampsite = campsites.find((item) => item.featured);
-    const featPromotion = promotions.find((item) => item.featured);
-    const featPartner = partners.find((item) => item.featured);
-
-    return (
-        <ScrollView>
-            <FeaturedItem item={featCampsite} />
-            <FeaturedItem item={featPromotion} />
-            <FeaturedItem item={featPartner} />
-        </ScrollView>
-    );
-};
-
-export default HomeScreen;  
+export default FeaturedItem;
