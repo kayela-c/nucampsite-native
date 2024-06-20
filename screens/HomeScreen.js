1e0183693064328c1bf31f27a780f4e77b4c9c30
Kayela Claybon <34044644+kayela-c@users.noreply.github.com>
2024-06-20 16:54:02 -0500

    replaced homeScreen code

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,45 +1,41 @@
-import { useState } from 'react'
+import { useState } from 'react';
 import {
     ScrollView,
     Text,
     View
-} from 'react-native-gesture-handler';
+} from 'react-native';
 import { Card } from 'react-native-elements';
-import { CAMPSITES } from '../shared/campsites'
-import { PROMOTIONS } from '../shared/promotions'
-import { PARTNERS } from '../shared/partners'
+import { CAMPSITES } from '../shared/campsites';
+import { PROMOTIONS } from '../shared/promotions';
+import { PARTNERS } from '../shared/partners';
 
 const FeaturedItem = ({ item }) => {
     if (item) {
         return (
             <Card containerStyle={{ padding: 0 }}>
                 <Card.Image source={item.image}>
-                    <View
-                        style={{ justifyContent: 'center', flex: 1 }}>
+                    <View style={{ justifyContent: 'center', flex: 1 }}>
                         <Text
                             style={{
                                 color: 'white',
-                                fontSize: 20,
                                 textAlign: 'center',
-                                backgroundColor: 'rgba(0, 0, 0, 0.5)'
-                                // fontWeight: 'bold',
-                                // padding: 10,
+                                fontSize: 20,
+                                backgroundColor: 'rgba(0, 0, 0, 0.5)',
+                                fontWeight: 'bold',
+                                padding: 10,
                             }}
                         >
                             {item.name}
                         </Text>
                     </View>
                 </Card.Image>
-                <Card.Divider />
-                <Text style={{ marginBottom: 20 }}>
-                    {item.description}
+                <Text style={{ margin: 20 }}>{item.description}
                 </Text>
             </Card>
         );
     }
-    return <View />
+    return <View />;
 };
-
 
 const HomeScreen = () => {
     const [campsites, setCampsites] = useState(CAMPSITES);
@@ -59,4 +55,4 @@ const HomeScreen = () => {
     );
 };
 
-export default HomeScreen;  
+export default HomeScreen;
