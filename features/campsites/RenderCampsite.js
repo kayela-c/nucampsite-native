bdea421bbaf9cd8ec4f73f5ec1fd29844652f3ba
Kayela Claybon <34044644+kayela-c@users.noreply.github.com>
2024-06-27 15:33:24 -0500

    update

--- features/campsites/RenderCampsite.js
+++ features/campsites/RenderCampsite.js
@@ -1,13 +1,15 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { StyleSheet, Text, View } from 'react-native'
 import {
-    Card
+    Card,
+    Icon
 } from 'react-native-elements'
 
-const RenderCampsite = ({ campsite }) => {
+const RenderCampsite = (props) => {
+    const { campsite, markFavorite } = props;
     if (campsite) {
         return (
-            <Card containerStyle={{ padding: 0 }}>
+            <Card containerStyle={StyleSheet.cardContainer}>
                 <Card.Image source={campsite.image}>
                     <View style={{ justifyContent: 'center', flex: 1 }}>
                         <Text style={{
@@ -21,11 +23,38 @@ const RenderCampsite = ({ campsite }) => {
                     </View>
                 </Card.Image>
                 <Text style={{ margin: 20 }}>{campsite.description}</Text>
+
+
+                <Icon
+                    name={props.isFavorite ? 'heart' : 'heart-o'}
+                    type='font-awesome'
+                    color='#f50'
+                    raised
+                    reverse
+                    onPress={() =>
+                        props.isFavorite
+                            ? console.log('Already set as a favorite')
+                            : props.markFavorite()
+                    }
+                />
+
+
             </Card>
         )
     }
     return <View />;
 };
+
+
+
+const styles = StyleSheet.create({
+    cardContainer: {
+        padding: 0,
+        margin: 0,
+        marginBottom: 20
+    }
+});
+
 
 
 export default RenderCampsite
