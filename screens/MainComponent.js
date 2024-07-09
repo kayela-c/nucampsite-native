1ba2fbef5ee05211318c437ad4cef80f67da28a6
Kayela Claybon <34044644+kayela-c@users.noreply.github.com>
2024-07-09 18:24:43 -0500

    feat register

--- screens/MainComponent.js
+++ screens/MainComponent.js
@@ -22,6 +22,7 @@ import { fetchPromotions } from '../features/promotions/promotionsSlice';
 import { fetchComments } from '../features/comments/commentsSlice';
 import FavoritesScreen from './FavoritesScreen';
 import LoginScreen from './LoginScreen';
+import { getFocusedRouteNameFromRoute } from '@react-navigation/core';
 
 const Drawer = createDrawerNavigator();
 
@@ -151,10 +152,16 @@ const LoginNavigator = () => {
             <Stack.Screen
                 name='Login'
                 component={LoginScreen}
-                options={({ navigation }) => ({
+                options={({ navigation, route }) => ({
+                    headerTitle: getFocusedRouteNameFromRoute(route),
                     headerLeft: () => (
                         <Icon
-                            name='sign-in'
+                            name={
+                                getFocusedRouteNameFromRoute(route) ===
+                                    'Register'
+                                    ? 'user-plus'
+                                    : 'sign-in'
+                            }
                             type='font-awesome'
                             iconStyle={styles.stackIcon}
                             onPress={() => navigation.toggleDrawer()}
@@ -234,7 +241,7 @@ const Main = () => {
             <Drawer.Navigator
                 initialRouteName='HomeDrawer'
                 drawerContent={CustomDrawerContent}
-                screenOptions={{ 
+                screenOptions={{
                     headerShown: false,
                     drawerStyle: { backgroundColor: '#CEC8FF' }
                 }}
